bea116d46404b56f2b4bb8fee86ab5c88906d33e
Peter <pnguyeddk@gmail.com>
2020-04-20 21:41:34 -0400

    Imported Nespaper and Feedparser libraries and printed out hello world to see if code works initially so far

--- news_agg.py
+++ news_agg.py
@@ -1,3 +1,16 @@
-import math.io
+import feedparser as fp
+import json
+import newspaper
+from newspaper import Article
+from time import mktime
+from datetime import datetime
+
+#set the limit for number of articles to download
+LIMIT = 5
+
+data = {}
+data['newspaper articles'] = {}
+
 
 print ("hello world!")
+
