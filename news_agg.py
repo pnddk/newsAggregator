dc7c3c359d04f20f100e7fdae719b741c73b7109
Peter <pnguyeddk@gmail.com>
2020-04-23 21:45:29 -0400

    added stub comments on what I want to do with my news aggregator program.

--- news_agg.py
+++ news_agg.py
@@ -1,3 +1,4 @@
+# This program is used to download various newspaper articles using a python script and a json file.
 import feedparser as fp
 import json
 import newspaper
@@ -6,11 +7,21 @@ from time import mktime
 from datetime import datetime
 
 #set the limit for number of articles to download
-LIMIT = 5
-
+LIMIT = 10
+"""
 data = {}
 data['newspaper articles'] = {}
-
+"""
 
 print ("hello world!")
+
+#load a json file with the list of newspaper websites using a switch case statement instead of for loops.
+
+#run a machine learning algorithm here...
+
+#add a training set of data for newspaper websites???? Maybe this can be other newspaper topics in the same vein...
+
+#add an optimization method to fine tune learning algorithm such as alpha-beta or markov tree decision. This specifically can contain weight functions as a for loop for accounting for different weight factors.
+
+
 
