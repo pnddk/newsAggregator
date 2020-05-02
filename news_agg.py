7faeec328771410cf5e877d7b97ab1d3a432f1ad
Peter <pnguyeddk@gmail.com>
2020-05-01 23:01:28 -0400

    added comments on what for loops to do and what functions it serves.

--- news_agg.py
+++ news_agg.py
@@ -8,14 +8,25 @@ from datetime import datetime
 
 #set the limit for number of articles to download
 LIMIT = 10
-"""
-data = {}
-data['newspaper articles'] = {}
-"""
 
+#creating a dict object called data, which stores newspaper articles in a json file and load into news_agg.py
+data = {}
+data['newspapers'] = []
+
+#stub
 print ("hello world!")
 
-#load a json file with the list of newspaper websites using a switch case statement instead of for loops.
+# load a json file with the list of newspaper websites using a switch case statement instead of for loops. As a reminder,
+# when the word "open" appears, it means you are loading a file with the name enclosed in appostrophes and this file will
+# remain open for as long as it is needed, which is done when using "with" since it acts as an encapsulated context manager.
+with open ('newspaper.txt') as json_file:
+    newspaper_corps = json.load(json_file)
+#run a for loop that looks for the name of the news organization, print it
+#run a for loop that looks for the title of the particular news article, print it
+#run a for loop that looks for the date of the article, print it
+#print the url of the article(s)
+
+
 
 #run a machine learning algorithm here...
 
