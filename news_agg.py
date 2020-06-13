90277c1446598167ef221ea98d736f6fa3ea5809
Peter <pnguyeddk@gmail.com>
2020-06-13 10:10:50 -0400

    Changed some syntax on newsite.json for consistency and incorporated a count to determine the current article that is being parsed through the for loop. Added said for loop in order to parse the json file to see if there is an RSS text included, of which case it will be used to print the link of the top rated news article.

--- news_agg.py
+++ news_agg.py
@@ -1,5 +1,6 @@
 # This program is used to download various newspaper articles using a python script and a json file.
-import feedparser as fp
+import feedparser
+import nltk
 import json
 import newspaper
 from newspaper import Article
@@ -10,8 +11,8 @@ from datetime import datetime
 LIMIT = 10
 
 #creating a dict object called data, which stores newspaper articles in a json file and load into news_agg.py
-data = {}
-data['newspapers'] = []
+dataEntry = {}
+dataEntry['newspapers'] = []
 
 #stub
 print ("hello world!")
@@ -19,10 +20,26 @@ print ("hello world!")
 # load a json file with the list of newspaper websites using a switch case statement instead of for loops. As a reminder,
 # when the word "open" appears, it means you are loading a file with the name enclosed in appostrophes and this file will
 # remain open for as long as it is needed, which is done when using "with" since it acts as an encapsulated context manager.
-with open ('newsite.txt') as json_file:
-    newspaper_corps = json.load(json_file)
-#run a for loop that looks for the name of the news organization, print it
-    for newspaper in 
+with open ('newsite.json') as json_file:
+    jsonData = json.load(json_file)
+#start with a count at 1, going up to 10 articles
+count = 1
+
+#the following line iterates through the newspaper companies and prints out tuple pairs of information using a Python Dict Method items() 
+#the following code checks for any value in the items() method in the json file, specifically the rss, publishing date and regular news article using if statements
+for newspaper, value in jsonData.items():
+    if 'rss' in value:
+        data = feedparser.parse(value['rss'])
+        print ("Downloading from: ", newspaper)
+        newsArticle = {
+                "rss": value['rss'],
+                "link": value['link'],
+                "articles": []
+        }
+
+       
+    
+
 #run a for loop that looks for the title of the particular news article, print it
 #run a for loop that looks for the date of the article, print it
 #print the url of the article(s)
